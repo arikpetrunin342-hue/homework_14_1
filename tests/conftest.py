af7b853044b9faaad939f048a53f5e0e0e35260f
Ярослав Петрунин <arikpetrunin342@gmail.com>
2026-07-28 12:24:19 +0300

    update: conftest.py, test_class_for_work.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,17 +1,25 @@
 import pytest
 
-from src.class_for_work import Product, Category
+from src.class_for_work import Category, Product
 
-product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+product1 = Product(
+    "Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5
+)
 product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
 product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
 
+
 @pytest.fixture
 def product():
-    return Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+    return Product(
+        "Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5
+    )
+
 
 @pytest.fixture
 def category():
-    return Category("Смартфоны",
-                         "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
-                         [product1, product2, product3])
+    return Category(
+        "Смартфоны",
+        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+        [product1, product2, product3],
+    )
--- tests/test_class_for_work.py
+++ tests/test_class_for_work.py
@@ -1,13 +1,15 @@
-
 def test_products(product):
     assert product.name == "Samsung Galaxy S23 Ultra"
     assert product.description == "256GB, Серый цвет, 200MP камера"
     assert product.price == 180000.0
     assert product.quantity == 5
 
+
 def test_category(category):
     assert category.name == "Смартфоны"
-    assert category.description == "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни"
+    assert (
+        category.description
+        == "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни"
+    )
     assert category.category_count == 1
     assert category.product_count == 3
-
